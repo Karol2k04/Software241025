664bff33e4b06e3ceb144407db5d05c43ab176e6
Karol <271504@student.pwr.edu.pl>
2025-10-24 06:18:40 +0200

    Fixed dividing by zero

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -15,6 +15,9 @@ public class Calculator {
     }
 
     public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
         return (double) a / b;
     }
 
